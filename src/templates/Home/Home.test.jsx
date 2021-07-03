eac446aa9a61108c2df86e614bef2930bb3d5840
João Bispo <joaobispo2077@gmail.com>
2021-07-03 13:21:39 -0300

    test: add should render Home

--- src/templates/Home/Home.test.jsx
+++ src/templates/Home/Home.test.jsx
@@ -2,6 +2,8 @@ import { render } from '@testing-library/react';
 import Home from '.';
 import { renderTheme } from '../../styles/themes/renderTheme';
 
-test('Home', () => {
-	render(renderTheme(<Home />));
+describe('<Home />', () => {
+	test('should render Home', () => {
+		render(renderTheme(<Home />));
+	});
 });
