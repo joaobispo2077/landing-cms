ab502ad68fd5c378e6390094e43934f75a4b54e6
João Bispo <joaobispo2077@gmail.com>
2021-07-03 12:21:12 -0300

    feat: add custom sizes into heading component

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import { Title } from './styles';
 import P from 'prop-types';
 
-export const Heading = ({ children, darken = false }) => {
-	return <Title darken={darken}>{children}</Title>;
+export const Heading = ({
+	children,
+	darken = false,
+	as = 'h1',
+	size = 'big',
+}) => {
+	return (
+		<Title as={as} size={size} darken={darken}>
+			{children}
+		</Title>
+	);
 };
 
 Heading.propTypes = {
-	children: P.oneOfType([P.string, P.node, P.element]).isRequired,
 	darken: P.bool,
+	as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+	size: P.oneOf(['small', 'medium', 'big', 'huge']),
+	children: P.oneOfType([P.string, P.node, P.element]).isRequired,
 };
--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 
+const titleSize = {
+	small: (theme) => theme.font.sizes.medium,
+	medium: (theme) => theme.font.sizes.large,
+	big: (theme) => theme.font.sizes.xlarge,
+	huge: (theme) => theme.font.sizes.xhuge,
+};
+
 export const Title = styled.h1`
 	color: ${({ theme, darken }) =>
 		darken ? theme.colors.dark : theme.colors.light};
+	font-size: ${({ size, theme }) => titleSize[size](theme)};
 `;
