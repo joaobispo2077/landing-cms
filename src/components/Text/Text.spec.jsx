e6a577a3683be6a9f5f48e22d02aa52ea6e893a9
João Bispo <joaobispo2077@gmail.com>
2021-07-04 12:49:51 -0300

    test: add should render, have correct size and snapshot into component text

--- src/components/Text/Text.spec.jsx
+++ src/components/Text/Text.spec.jsx
@@ -1,12 +1,38 @@
 import { render, screen } from '@testing-library/react';
 import { Text } from '.';
 import { renderTheme } from '../../styles/themes/renderTheme';
-// import { themes } from '../../styles/themes';
+import { themes } from '../../styles/themes';
 
 describe('<Text/>', () => {
 	it('should render', () => {
 		render(renderTheme(<Text>Texto</Text>));
 
-		expect(screen.getByRole('heading')).toBeInTheDocument();
+		expect(screen.getByText('Texto')).toBeInTheDocument();
+	});
+
+	it('should have correct size', () => {
+		const { container } = render(renderTheme(<Text>Texto</Text>));
+
+		const text = container.querySelector('p');
+
+		expect(text).toHaveStyle({
+			'font-size': themes['default'].font.sizes.medium,
+		});
+	});
+
+	it('should match with snapshot', () => {
+		const { container } = render(renderTheme(<Text>Texto</Text>));
+
+		expect(container.firstChild).toMatchInlineSnapshot(`
+		.c0 {
+		  font-size: 2.4rem;
+		}
+
+		<p
+		  class="c0"
+		>
+		  Texto
+		</p>
+	`);
 	});
 });
