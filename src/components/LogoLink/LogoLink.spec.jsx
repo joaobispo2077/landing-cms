c391ef24b5889f27473ea9234050f5c5db089368
João Bispo <joaobispo2077@gmail.com>
2021-07-18 15:24:45 -0300

    test: ensure render LogoLink and match with snapshot

--- src/components/LogoLink/LogoLink.spec.jsx
+++ src/components/LogoLink/LogoLink.spec.jsx
@@ -4,9 +4,37 @@ import { renderTheme } from '../../styles/themes/renderTheme';
 // import { themes } from '../../styles/themes';
 
 describe('<LogoLink/>', () => {
-	it('should render', () => {
-		render(renderTheme(<LogoLink>Texto</LogoLink>));
+	it('should render text logo', () => {
+		render(renderTheme(<LogoLink link="#target" text="Hello World" />));
 
-		expect(screen.getByRole('heading')).toBeInTheDocument();
+		const heading = screen.getByRole('heading', { name: 'Hello World' });
+
+		expect(heading).toBeInTheDocument();
+		expect(heading.firstChild).toHaveAttribute('href', '#target');
+	});
+
+	it('should render image logo', () => {
+		render(
+			renderTheme(
+				<LogoLink link="#target" text="Hello World" src="image.jpg" />,
+			),
+		);
+
+		const img = screen.getByRole('img', {
+			name: 'Hello World',
+		});
+
+		expect(img).toBeInTheDocument();
+		expect(img).toHaveAttribute('src', 'image.jpg');
+	});
+
+	it('should match with snapshot', () => {
+		const { container } = render(
+			renderTheme(
+				<LogoLink link="#target" text="Hello World" src="image.jpg" />,
+			),
+		);
+
+		expect(container.firstChild).toMatchSnapshot();
 	});
 });
