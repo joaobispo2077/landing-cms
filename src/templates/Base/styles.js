997b262bd56837b7d00b9c0d36507674f2a93bb5
João Bispo <joaobispo2077@gmail.com>
2021-07-25 15:36:12 -0300

    refactor: adjust footer with Base template

--- src/templates/Base/styles.js
+++ src/templates/Base/styles.js
@@ -1,5 +1,11 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-	${() => css``}
+	${({ theme }) => css`
+		padding-top: 5.4rem;
+
+		@media ${theme.media.lteMedium} {
+			padding-top: 0;
+		}
+	`}
 `;
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,14 +1,10 @@
-import { Menu } from '../../components/Menu';
-import * as Styled from './styles';
+// import { Menu } from '../../components/Menu';
+// import * as Styled from './styles';
+import { Base } from '../Base';
+import { mockBase } from '../Base/stories';
 
 function Home() {
-	return (
-		<div>
-			<Styled.Wrapper>
-				<Menu />
-			</Styled.Wrapper>
-		</div>
-	);
+	return <Base {...mockBase} />;
 }
 
 export default Home;
