4537e04bf52fbf75adef73d3e41e1e7ed9740e0c
João Bispo <joaobispo2077@gmail.com>
2021-07-25 14:11:44 -0300

    feat: add GridImage component

---
+++ src/components/GridImage/styles.js
@@ -0,0 +1,38 @@
+import styled, { css } from 'styled-components';
+
+import { Container as TextContainer } from '../Text/styles';
+
+export const Container = styled.div`
+	${({ theme }) => css`
+		${TextContainer} {
+			margin-bottom: ${theme.spacings.large};
+		}
+	`}
+`;
+
+export const Grid = styled.div`
+	${({ theme }) => css`
+		counter-reset: grid-counter;
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+		gap: ${theme.spacings.large};
+	`}
+`;
+
+export const GridElement = styled.div`
+	${() => css`
+		overflow: hidden;
+	`}
+`;
+
+export const Image = styled.img`
+	${() => css`
+		width: 100%;
+		cursor: pointer;
+		transition: transform 0.25s ease-in-out;
+
+		&:hover {
+			transform: scale(1.2) rotate(10deg);
+		}
+	`}
+`;
