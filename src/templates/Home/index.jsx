24c20bd7dbe8bb54fca6aab42d309aef8a82c5ae
João Bispo <joaobispo2077@gmail.com>
2021-07-27 12:10:47 -0300

    feat: add simple 404 (not found) page

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -3,26 +3,37 @@
 import { useEffect, useState } from 'react';
 import { Base } from '../Base';
 import mockBase from '../Base/mock';
+
+import { NotFound } from '../NotFound';
 import { mapData } from '../../api/index';
+
 function Home() {
-	const [pageData, setPageData] = useState(null);
+	const [pageData, setPageData] = useState(undefined);
 
 	const fetchPageData = async () => {
-		const response = await fetch(
-			'http://localhost:1337/pages/?slug=landing-page',
-		);
-		const data = await response.json();
+		try {
+			const response = await fetch(
+				'http://localhost:1337/pages/?slug=landing-page',
+			);
+			const data = await response.json();
 
-		const page = mapData(data[0]);
-		setPageData(page);
-		console.log(page);
+			const page = mapData(data[0]);
+			setPageData(page);
+			console.log(page);
+		} catch (error) {
+			setPageData(undefined);
+		}
 	};
 
 	useEffect(() => {
 		fetchPageData();
 	}, []);
 
-	if (pageData === null) {
+	if (pageData === undefined) {
+		return <NotFound />;
+	}
+
+	if (pageData && !pageData.slug) {
 		return <h1>Carregando...</h1>;
 	}
 
