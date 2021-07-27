5e8dc3ffac5ba5e1f003a53a5dd5fa045ef7fd1f
João Bispo <joaobispo2077@gmail.com>
2021-07-27 10:11:41 -0300

    chore: clean tests

--- src/templates/Home/Home.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import Home from '.';
-import { renderTheme } from '../../styles/themes/renderTheme';
-
-describe('<Home />', () => {
-	test('should render Home', () => {
-		render(renderTheme(<Home />));
-	});
-});
